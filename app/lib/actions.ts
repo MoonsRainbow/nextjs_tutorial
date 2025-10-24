d52bb22d93dcfdf48deb6be3b5be076243592d98
K-JunHo <kalmia6863@gmail.com>
2025-10-24 16:05:23 +0900

    Chapter 13: Handling Errors. (Return removed at actions.ts)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,9 +34,6 @@ export async function createInvoice(formData: FormData) {
 		`;
 	} catch (error) {
 		console.log(error);
-		return {
-			message: "Database Error: Failed to Create Invoice.",
-		};
 	}
 
 	revalidatePath("/dashboard/invoices");
@@ -60,9 +57,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 		`;
 	} catch (error) {
 		console.log(error);
-		return {
-			message: "Database Error: Failed to Update Invoice.",
-		};
 	}
 
 	revalidatePath("/dashboard/invoices");
